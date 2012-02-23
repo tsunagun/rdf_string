7242687d23a688c8ea68c467e26d0166e52b9600
tsuna <tsuna@slis.tsukuba.ac.jp>
2012-02-23 11:22:32 +0900

    Spec for extracting Microdata

--- spec/rdf_string_spec.rb
+++ spec/rdf_string_spec.rb
@@ -19,4 +19,22 @@ describe RDFString do
 			end
 		end
 	end
+	describe "Microdata" do
+		before do
+			@uri = "http://linter.structured-data.org/examples/google-rs/review.md.html"
+			@str = open(@uri).read
+		end
+		describe "microdata?" do
+			subject { @str.microdata? }
+			it "be true" do
+				should be_true
+			end
+		end
+		describe 'microdata' do
+			subject { @str.microdata(@uri) }
+			it "be triples" do
+				should have_at_least(1).triples
+			end
+		end
+	end
 end
