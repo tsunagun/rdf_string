a0e8461361207d53b319ce4d5a3d8b57c16eed73
tsuna <tsuna@slis.tsukuba.ac.jp>
2012-02-23 12:29:34 +0900

    Spec for extracting DC-HTML (RFC2731)

--- spec/rdf_string_spec.rb
+++ spec/rdf_string_spec.rb
@@ -55,4 +55,16 @@ describe RDFString do
 			end
 		end
 	end
+	describe "DC-HTML" do
+		before do
+			@uri = "http://www.aozora.gr.jp/cards/000020/files/2569_28291.html"
+			@str = open(@uri).read
+		end
+		describe "dc_html" do
+			subject { @str.dc_html(@uri) }
+			it "be triples" do
+				should have_at_least(1).triples
+			end
+		end
+	end
 end
